f3f2b20b8290aeafcc78203752ccfa0f51d5d32c
sashi <georgievaleks854@gmail.com>
2024-03-19 17:42:36 +0200

    Removed Phonenumber property

--- JobListing.Infrastructure/Data/Models/Application.cs
+++ JobListing.Infrastructure/Data/Models/Application.cs
@@ -1,6 +1,7 @@
 ﻿using JobListing.Infrastructure.Data.Enums;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using static JobListing.Infrastructure.Constants.DataConstants;
 
 namespace JobListing.Infrastructure.Data.Models
 {
@@ -8,13 +9,13 @@ namespace JobListing.Infrastructure.Data.Models
     {
         public int Id { get; set; }
         [Required]
+        [MaxLength(FirstNameMaxLength)]
         public string FirstName { get; set; } = string.Empty;
         [Required]
+        [MaxLength(LastNameMaxLength)]
         public string LastName { get; set; } = string.Empty;
         [Required]
         public string Email { get; set; } = string.Empty;
-        [Required]
-        public string Phonenumber { get; set; } = string.Empty;
         [Required]
         public string Resume { get; set; } = string.Empty;
         [DefaultValue(ApplicationStatus.Pending)]
